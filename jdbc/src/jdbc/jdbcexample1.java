ad92e1d95026a88b259839ce1f08a8de0839d15a
abhishekhebbar173 <82367194+abhishekhebbar173@users.noreply.github.com>
2021-05-15 19:49:09 +0530

    Update jdbcexample1.java

--- jdbc/src/jdbc/jdbcexample1.java
+++ jdbc/src/jdbc/jdbcexample1.java
@@ -12,6 +12,7 @@ public class jdbcexample1 {
 		//This file is edited
 		//2nd time edited
 		//1 st time pushing
+		//third time edited
 		Driver driver=new com.mysql.cj.jdbc.Driver();
 		DriverManager.registerDriver(driver);
 		String URL="jdbc:mysql://localhost:3306/?user=root";
